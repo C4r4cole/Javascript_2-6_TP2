32e438fb875dba6fc9760450d4f192cc717e191b
Caracole <francoismoulin37@gmail.com>
2022-09-09 18:01:17 +0200

    TP2 fini

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 // Créer un événément au scroll
 
+let lastScroll = 0;
+
+window.addEventListener("scroll", () => {
+  if (lastScroll < window.scrollY) {
+    navbar.style.top = "-60px";
+  } else {
+    navbar.style.top = "0px";
+  }
+  lastScroll = window.scrollY;
+});
+
 // Cacher la navbar si l'utilisateur comment à descendre et la sortir quand il remonte
 
 // Stocker la valeur du précédent niveau de scroll pour savoir si l'on est monté ou descendu
